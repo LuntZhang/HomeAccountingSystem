eabc166b4f88c80796ed91e625eb063f331faee8
张龙涛 <zlt389815117outlook.com>
2019-05-10 12:37:52 +0800

    --添加登录页

---
+++ HomeAccountingSystem/HomeAccountingSystem/Login.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HomeAccountingSystem
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelCopyright = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tzxButton1 = new TZXControlLibrary.ButtonEx.TzxButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panelLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.BackgroundImage = global::HomeAccountingSystem.Properties.Resources._9bc7f20ad85f3b1c33e6c180972348ea;
+            this.panelLogin.Controls.Add(this.textBox2);
+            this.panelLogin.Controls.Add(this.textBox1);
+            this.panelLogin.Controls.Add(this.label1);
+            this.panelLogin.Controls.Add(this.tzxButton1);
+            this.panelLogin.Controls.Add(this.labelCopyright);
+            this.panelLogin.Controls.Add(this.label3);
+            this.panelLogin.Controls.Add(this.label7);
+            this.panelLogin.Controls.Add(this.label2);
+            this.panelLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLogin.Location = new System.Drawing.Point(0, 0);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(977, 550);
+            this.panelLogin.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label7.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label7.Location = new System.Drawing.Point(276, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(428, 65);
+            this.label7.TabIndex = 58;
+            this.label7.Text = "家庭记账管理系统";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.labelCopyright.AutoSize = true;
+            this.labelCopyright.BackColor = System.Drawing.Color.Transparent;
+            this.labelCopyright.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.labelCopyright.ForeColor = System.Drawing.Color.Black;
+            this.labelCopyright.Location = new System.Drawing.Point(381, 527);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(229, 17);
+            this.labelCopyright.TabIndex = 62;
+            this.labelCopyright.Text = "版权所有 © 2019 LuntZhang  版本V1.0 ";
+            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.label3.Location = new System.Drawing.Point(620, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 21);
+            this.label3.TabIndex = 65;
+            this.label3.Text = "密码：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.label2.Location = new System.Drawing.Point(620, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 21);
+            this.label2.TabIndex = 64;
+            this.label2.Text = "账号：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(152)))), ((int)(((byte)(96)))));
+            this.label1.Location = new System.Drawing.Point(618, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 31);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "用户登录";
+            // 
+            // tzxButton1
+            // 
+            this.tzxButton1.BackColor = System.Drawing.Color.Transparent;
+            this.tzxButton1.BackImg = null;
+            this.tzxButton1.BacklightLTRB = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.tzxButton1.Location = new System.Drawing.Point(614, 345);
+            this.tzxButton1.Name = "tzxButton1";
+            this.tzxButton1.Size = new System.Drawing.Size(90, 44);
+            this.tzxButton1.TabIndex = 66;
+            this.tzxButton1.Text = "登录";
+            this.tzxButton1.Tip = "";
+            this.tzxButton1.TipColor = System.Drawing.Color.Red;
+            this.tzxButton1.TotalPicCount = 5;
+            this.tzxButton1.UseDefaultBg = true;
+            this.tzxButton1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.textBox1.Location = new System.Drawing.Point(684, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 29);
+            this.textBox1.TabIndex = 67;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.textBox2.Location = new System.Drawing.Point(684, 295);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 29);
+            this.textBox2.TabIndex = 68;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 550);
+            this.Controls.Add(this.panelLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "登录";
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelCopyright;
+        private TZXControlLibrary.ButtonEx.TzxButton tzxButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
