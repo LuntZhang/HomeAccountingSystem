7400c8acd99d2de49f13879724cabed154424e44
张龙涛 <zlt389815117outlook.com>
2019-05-10 21:46:47 +0800

    --添加业务逻辑BLL

---
+++ HomeAccountingSystem/HomeAccountingSystem/BLL/jt_ys_zm.cs
@@ -0,0 +1,159 @@
+﻿/** 
+* jt_ys_zm.cs
+*
+* 功 能： N/A
+* 类 名： jt_ys_zm
+*
+* Ver    变更日期             负责人  变更内容
+* ───────────────────────────────────
+* V0.01  2019-05-10 21:15:55   N/A    初版        
+*/
+using System;
+using System.Data;
+using System.Collections.Generic;   
+using HomeAccountingSystem.Model;
+namespace HomeAccountingSystem.BLL
+{
+	/// <summary>
+	/// 预算账目表
+	/// </summary>
+	public partial class jt_ys_zm
+	{
+		private readonly HomeAccountingSystem.DAL.jt_ys_zm dal=new HomeAccountingSystem.DAL.jt_ys_zm();
+		public jt_ys_zm()
+		{}
+		#region  BasicMethod
+
+		/// <summary>
+		/// 得到最大ID
+		/// </summary>
+		public int GetMaxId()
+		{
+			return dal.GetMaxId();
+		}
+
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(int pk)
+		{
+			return dal.Exists(pk);
+		}
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public bool Add(HomeAccountingSystem.Model.jt_ys_zm model)
+		{
+			return dal.Add(model);
+		}
+
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(HomeAccountingSystem.Model.jt_ys_zm model)
+		{
+			return dal.Update(model);
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(int pk)
+		{
+			
+			return dal.Delete(pk);
+		}
+
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public HomeAccountingSystem.Model.jt_ys_zm GetModel(int pk)
+		{
+			
+			return dal.GetModel(pk);
+		}
+
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			return dal.GetList(strWhere);
+		}
+		/// <summary>
+		/// 获得前几行数据
+		/// </summary>
+		public DataSet GetList(int Top,string strWhere,string filedOrder)
+		{
+			return dal.GetList(Top,strWhere,filedOrder);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<HomeAccountingSystem.Model.jt_ys_zm> GetModelList(string strWhere)
+		{
+			DataSet ds = dal.GetList(strWhere);
+			return DataTableToList(ds.Tables[0]);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<HomeAccountingSystem.Model.jt_ys_zm> DataTableToList(DataTable dt)
+		{
+			List<HomeAccountingSystem.Model.jt_ys_zm> modelList = new List<HomeAccountingSystem.Model.jt_ys_zm>();
+			int rowsCount = dt.Rows.Count;
+			if (rowsCount > 0)
+			{
+				HomeAccountingSystem.Model.jt_ys_zm model;
+				for (int n = 0; n < rowsCount; n++)
+				{
+					model = dal.DataRowToModel(dt.Rows[n]);
+					if (model != null)
+					{
+						modelList.Add(model);
+					}
+				}
+			}
+			return modelList;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetAllList()
+		{
+			return GetList("");
+		}
+
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			return dal.GetRecordCount(strWhere);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			return dal.GetListByPage( strWhere,  orderby,  startIndex,  endIndex);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		//public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		//{
+			//return dal.GetList(PageSize,PageIndex,strWhere);
+		//}
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
---
+++ HomeAccountingSystem/HomeAccountingSystem/BLL/jt_zc_lx.cs
@@ -0,0 +1,156 @@
+﻿/**  
+* jt_zc_lx.cs
+*
+* 功 能： N/A
+* 类 名： jt_zc_lx
+*
+* Ver    变更日期             负责人  变更内容
+* ───────────────────────────────────
+* V0.01  2019-05-10 21:15:55   N/A    初版  
+*/
+using System;
+using System.Data;
+using System.Collections.Generic;            
+using HomeAccountingSystem.Model;
+namespace HomeAccountingSystem.BLL
+{
+	/// <summary>
+	/// 支出类型表
+	/// </summary>
+	public partial class jt_zc_lx
+	{
+		private readonly HomeAccountingSystem.DAL.jt_zc_lx dal=new HomeAccountingSystem.DAL.jt_zc_lx();
+		public jt_zc_lx()
+		{}
+		#region  BasicMethod
+
+		/// <summary>
+		/// 得到最大ID
+		/// </summary>
+		public int GetMaxId()
+		{
+			return dal.GetMaxId();
+		}
+
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(int pk)
+		{
+			return dal.Exists(pk);
+		}
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public bool Add(HomeAccountingSystem.Model.jt_zc_lx model)
+		{
+			return dal.Add(model);
+		}
+
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(HomeAccountingSystem.Model.jt_zc_lx model)
+		{
+			return dal.Update(model);
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(int pk)
+		{
+			
+			return dal.Delete(pk);
+		}   
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public HomeAccountingSystem.Model.jt_zc_lx GetModel(int pk)
+		{
+			
+			return dal.GetModel(pk);
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			return dal.GetList(strWhere);
+		}
+		/// <summary>
+		/// 获得前几行数据
+		/// </summary>
+		public DataSet GetList(int Top,string strWhere,string filedOrder)
+		{
+			return dal.GetList(Top,strWhere,filedOrder);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<HomeAccountingSystem.Model.jt_zc_lx> GetModelList(string strWhere)
+		{
+			DataSet ds = dal.GetList(strWhere);
+			return DataTableToList(ds.Tables[0]);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<HomeAccountingSystem.Model.jt_zc_lx> DataTableToList(DataTable dt)
+		{
+			List<HomeAccountingSystem.Model.jt_zc_lx> modelList = new List<HomeAccountingSystem.Model.jt_zc_lx>();
+			int rowsCount = dt.Rows.Count;
+			if (rowsCount > 0)
+			{
+				HomeAccountingSystem.Model.jt_zc_lx model;
+				for (int n = 0; n < rowsCount; n++)
+				{
+					model = dal.DataRowToModel(dt.Rows[n]);
+					if (model != null)
+					{
+						modelList.Add(model);
+					}
+				}
+			}
+			return modelList;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetAllList()
+		{
+			return GetList("");
+		}
+
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			return dal.GetRecordCount(strWhere);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			return dal.GetListByPage( strWhere,  orderby,  startIndex,  endIndex);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		//public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		//{
+			//return dal.GetList(PageSize,PageIndex,strWhere);
+		//}
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
